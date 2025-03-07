0c8001fdc213184e744014c64790abb0317f5d57
tljibbers <aahijoh@gmail.com>
2025-03-06 19:45:52 -0800

    Update main-gui.py

--- main-gui.py
+++ main-gui.py
@@ -1,14 +1,18 @@
 import tkinter as tk
+from tkinter import filedialog
+from tkinter import Button
 import speech_recognition as sr
-import pyaudio as pa
+import pyaudio
 
+switcher = False
 def workAudio(event):
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
         label1 = tk.Label(root, text="Talk into your microphone")
         label1.pack()
-        audio_text = r.listen(source)
+        r.adjust_for_ambient_noise(source, 0.2)
+        audio_text = r.listen(source, 0, 10)
         label2 = tk.Label(root, text="It's over")
         label2.pack()
 
@@ -19,9 +23,31 @@ def workAudio(event):
             label4 = tk.Label(root, text="couldn't understand that")
             label4.pack()
 
+def audioFileInput():
+    file_path = filedialog.askopenfilename()
+    if(file_path and checkIfAudioFile(file_path)):
+        r = sr.Recognizer()
+
+        with sr.AudioFile(file_path) as source:
+            audio = r.record(source, 90)
+
+        text = r.recognize_google(audio)
+        print(text)
+
+def checkIfAudioFile(file):
+    if(file.lower().endswith('.wav')):
+        return True
+    if(file.lower().endswith('.mp3')):
+        return True
+
+
+
 root = tk.Tk('Test', 'TestAgain', 'TestWindow');
+root.geometry('300x300')
 labeltest = tk.Label(root, text='Press f to record your audio')
 labeltest.pack()
+button = Button(root, text='Add Audio File (Wav, Mp3)', command=lambda: audioFileInput())
+button.pack()
 root.bind("<KeyPress>", workAudio)
 root.mainloop();
 
